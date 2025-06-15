389dd84bacb669de76545cbe24f98fd89df67d42
mine0059 <onojefemuemine@gmail.com>
2025-06-15 10:08:54 +0100

    integrated reusable components fot notebook and notes

--- src/js/components/Tooltip.js
+++ src/js/components/Tooltip.js
@@ -3,3 +3,26 @@
  */
 
 'use strict';
+
+export const Tooltip = function ($element) {
+    const /** {HTMLElenent} */ $tooltip = document.createElement('span');
+    $tooltip.classList.add('tooltip', 'text-body-small');
+
+    $element.addEventListener('mouseenter', function () {
+       $tooltip.textContent =  this.dataset.tooltip;
+
+       const {
+        top,
+        left,
+        height,
+        width
+       } = this.getBoundingClientRect();
+
+       $tooltip.style.top = top + height + 4 + 'px';
+       $tooltip.style.left = left + (width / 2) + 'Px';
+       $tooltip.style.transform = 'translate(-50% 0)';
+       document.body.appendChild($tooltip);
+    });
+
+    $element.addEventListener('mouseleave', $tooltip.remove.bind($tooltip));
+}
